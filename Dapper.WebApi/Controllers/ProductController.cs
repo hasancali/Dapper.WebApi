6f6e90dc74b087dbaa473cc71c1c787ed6d29e49
Hasan Çalı <72003584+hasancali@users.noreply.github.com>
2022-06-05 15:22:59 +0300

    Project Create

---
+++ Dapper.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Application.Interfaces;
+using Dapper.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper.WebApi.Controllers
+{
+    [Route("api/v1/Dapper.WebApi/[Controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(Product product)
+        {
+            var data = await unitOfWork.Products.InsertAsync(product);
+            return Ok(data);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(Product product)
+        {
+            var data = await unitOfWork.Products.UpdateAsync(product);
+            return Ok(data);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var data = await unitOfWork.Products.DeleteAsync(id);
+            return Ok(data);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await unitOfWork.Products.GetAllAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await unitOfWork.Products.GetByIdAsync(id);
+            if (data == null) return Ok();
+            return Ok(data);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Dapper.WebApi
+
+## Product 
+Controller for main model
+
+* **Method:**
+ `POST`
+​**/api​/v1​/Dapper.WebApi​/Product​/Create**
+Create model with dapper
+
+* **Method:**
+ `PUT`
+**​​/api​/v1​/Dapper.WebApi​/Product​/Update**
+Update model with dapper
+
+* **Method:**
+ `DELETE`
+**​​/api​/v1​/Dapper.WebApi​/Product​/Delete​/{id}**
+Delete model with dapper
+
+* **Method:**
+ `GET`
+**​/api​/v1​/Dapper.WebApi​/Product​/GetAll**
+Fetch all records with dapper
+
+* **Method:**
+ `GET`
+**​​/api​/v1​/Dapper.WebApi​/Product​/GetById​/{id}**
+Fetch record with given id with dapper
